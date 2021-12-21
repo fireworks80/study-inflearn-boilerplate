9cc5ed771aa6f12ccd395ced85fd4f22992ae950
hanseungchul <seungchul-han@nhn-commerce.com>
2021-12-21 16:33:55 +0900

    connect mongodb

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_HOST).then(() => {
+	console.log('Connected Mongodb...');
+}).catch(error => {
+	console.error(error);
+});
 
 app.get('/', (req, res) => res.send('hello world'));
 
