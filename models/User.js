60e52ebead9cb5cecc8004975ee319be1b28cff6
hanseungchul <seungchul-han@nhn-commerce.com>
2021-12-21 17:57:14 +0900

    bcrypt를 이용한 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -30,6 +33,29 @@ const userSchema = mongoose.Schema({
 	tokenExp: {
 		type: Number
 	}
+});
+
+// user model에 user 정보를 저장하기 전에 cb를 한다.
+// 그 다음 ./src/index.js에서 user.save()로 간다
+userSchema.pre('save', function (next) {
+	const user = this;
+
+	if (!user.isModified('password')) return next();
+
+	// saltRountd를 이용해서 salt 생성
+	bcrypt.genSalt(saltRounds, (err, salt) => {
+		if (err) return next(err);
+
+		// salt를 이용해서 비밀 번호를 암호화 시킨다.
+		bcrypt.hash(user.password, salt, (err, hash) => {
+			if (err) return next(err);
+
+			user.password = hash;
+
+			// ./src/index.js로 보낸다
+			next();
+		});
+	});
 });
 
 const User = mongoose.model('User', userSchema);
